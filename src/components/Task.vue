cab3cd5c2c610621282e7f11dffeab93ec5d47c3
minamaher58 <minamahertalaat58@gmail.com>
2021-06-02 23:34:30 +0200

    adding tasks and toggle reminder

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="task">
-        <h3>{{ task.text }}</h3>
+    <div @dblclick="$emit('toggle-reminder', task.id)" :class="[task.reminder ? 'reminder' : '', 'task']">
+        <h3>{{ task.text }}
+            <i @click="$emit('delete-task', task.id)" class="fas fa-times"></i>
+        </h3>
         <p>{{ task.day }}</p>
     </div>
 </template>
@@ -11,10 +13,15 @@ export default {
     props: {
         task: Object
     },
+    methods: {
+    }
 }
 </script>
 
 <style scoped>
+    .fas {
+        color: red;
+    }
     .task {
         background: #f4f4f4;
         margin: 5px;
